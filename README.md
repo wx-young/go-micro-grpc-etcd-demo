ee7b61c949177633ad6214bc6605554c4d32edc9
Young <xiuyoung89@163.com>
2020-08-25 22:54:08 +0800

    add some config

---
+++ README.md
@@ -0,0 +1,8 @@
+#go-micro-grpc-etcd-demo
+
+```bash
+ll
+```
+
+## protoc 使用日志
+
--- gen_proto.sh
+++ gen_proto.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
 protoc --go_out=protobuf/greet/greet.proto=internal/proto/greet/greet.proto \
+       --go_path
